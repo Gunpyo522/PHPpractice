d36bcc55383999641639e2d8530fbd87d941db0f
Gunpyo522 <rjsvy0522@naver.com>
2023-09-12 15:46:56 +0900

    0912 웹마스터과정

---
+++ string_merge.php
@@ -0,0 +1,12 @@
+<?php
+    $num1 = "010";
+    $num2 = "5877";
+    $num3 = "4485";
+    $phone_num = $num1. "-".$num2."-".$num3;
+    echo "휴대폰 번호 : $phone_num"."<br>";
+
+    $email1 = "canyun6676";
+    $email2 = "gmail.com";
+    $email = $email1."@".$email2;
+    echo "이메일 주소 : $email";
+?>
