561137ee8e681cd70787c704c5c1819dd6ef20d6
Gunpyo522 <rjsvy0522@naver.com>
2023-09-12 15:32:35 +0900

    태용이와 태용이

---
+++ echo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h3>
+        <?php
+        echo '태용이와 태용이';
+        ?>
+        </h3>
+        <?php
+        $filename = 'taeyoung.jpg';
+        echo "<img src = '$filename'>";
+        echo "<br>";
+
+        $filename = 'taeyoung2.jpg';
+        echo "<img src = '$filename'>";
+        ?>
+    </body>
+</html>
